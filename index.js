612683ec4964525878dec4ae864fc6eaa696806e
jordan-urtoday <jordanliao@urtoday.com>
2024-06-13 13:20:47 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+// route
+import staffApiRoute from './ApiRoutes/staff.js';
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+const connect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log("Connected to mongoDB");
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+mongoose.connection.on('connected', () => {
+    console.log("MongoDB connected");
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log("MongoDB disconnected");
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    connect();
+    console.log(`app listening on port ${port}`);
+})
+
+// middlewares
+app.use('/api/v1/staff', staffApiRoute);
